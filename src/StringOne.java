320f56c00ca00e42cfb301f9bf5e84eeed41c2b1
manoj <manojs121@rediffmail.com>
2020-06-29 12:21:33 +0530

    29-06

---
+++ src/StringOne.java
@@ -0,0 +1,18 @@
+
+public class StringOne {
+
+    public static void main(String[] args) {
+        
+       String s1=new String("indore");
+       String s2=new String("indore");
+       String s3="India";
+       String s4="India";
+       
+       boolean res1=(s1==s2);
+       boolean res2=(s3==s4);
+       
+       System.out.println(res1);
+       System.out.println(res2);
+       
+    }
+}
